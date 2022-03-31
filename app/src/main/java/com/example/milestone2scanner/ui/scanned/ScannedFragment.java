aa450f3f276f4d3b8665b0702c2181db14f8979d
yixxx263 <yixxx263@umn.edu>
2022-03-30 22:55:22 -0500

    Second Commit

--- app/src/main/java/com/example/milestone2scanner/ui/scanned/ScannedFragment.java
+++ app/src/main/java/com/example/milestone2scanner/ui/scanned/ScannedFragment.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import com.example.milestone2scanner.R;
 
@@ -22,6 +25,20 @@ public class ScannedFragment extends Fragment {
                 new ViewModelProvider(this).get(ScannedViewModel.class);
         View root = inflater.inflate(R.layout.fragment_scanned, container, false);
         //final TextView textView = root.findViewById((R.id.text_scanned));
+
+        Button button1 = (Button) root.findViewById(R.id.GoHome);
+        button1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Navigation.findNavController(v).navigate(R.id.action_nav_scanned_to_nav_home);
+            }
+        });
+
+        Button button2 = (Button) root.findViewById(R.id.ScanAgain);
+        button2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Navigation.findNavController(v).navigate(R.id.action_nav_scanned_to_nav_scanner);
+            }
+        });
 
         return root;
     }
--- app/src/main/java/com/example/milestone2scanner/ui/scanner/ScannerFragment.java
+++ app/src/main/java/com/example/milestone2scanner/ui/scanner/ScannerFragment.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.milestone2scanner.R;
 
@@ -23,6 +28,14 @@ public class ScannerFragment extends Fragment {
                 new ViewModelProvider(this).get(ScannerViewModel.class);
         View root = inflater.inflate(R.layout.fragment_scanner, container, false);
 
+        ImageButton button = (ImageButton) root.findViewById(R.id.takePicture);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Navigation.findNavController(v).navigate(R.id.action_nav_scanner_to_nav_scanned);
+            }
+        });
+
         return root;
     }
+
 }
